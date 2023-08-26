828fd4df4a36aaf9a7c546feb16cbc8d97160da4
engysaber0 <141266624+engysaber0@users.noreply.github.com>
2023-08-26 21:37:51 +0300

    Add files via upload

---
+++ Session4/rename_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+extension="$1"
+new_name="$2"
+counter=1
+for file in *."$extension";
+do
+new_file="${new_name}${counter}.${extension}"
+mv "$file" "$new_file"
+counter=$((counter + 1))
+done
